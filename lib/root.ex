13b022dcade0435ee195066de468318fb9879ce1
Santiago Imelio <santiago.imelio@gmail.com>
2024-06-29 23:41:02 -0300

    newton-raphson

--- lib/root.ex
+++ lib/root.ex
@@ -2,4 +2,27 @@ defmodule Metnum.Root do
   @moduledoc """
   Methods for approximating the real-valued roots of a function.
   """
+
+  import Nx.Defn
+
+  def newton_raphson(func, x_0, epochs) do
+    newton_raphson_aux(func, Nx.tensor([x_0]), 0, epochs)
+  end
+
+  defp newton_raphson_aux(_func, seq, curr_epoch, epochs) when curr_epoch == epochs do
+    seq
+  end
+
+  defp newton_raphson_aux(func, seq, curr_epoch, epochs) do
+    seq_curr = newton_raphson_step(func, seq, curr_epoch)
+    newton_raphson_aux(func, seq_curr, curr_epoch + 1, epochs)
+  end
+
+  defnp newton_raphson_step(func, seq, curr_epoch) do
+    x_prev = seq[curr_epoch]
+    func_grad = grad(func)
+    x_curr = x_prev - func.(x_prev) / func_grad.(x_prev)
+
+    Nx.concatenate([seq, Nx.broadcast(x_curr, {1})])
+  end
 end
---
+++ test/root_test.exs
@@ -0,0 +1,29 @@
+defmodule Metnum.RootTest do
+  use Metnum.Case
+
+  alias Metnum.Root, as: R
+
+  describe "#newton_raphson/3" do
+    test "1 variable function" do
+      # f(x) = x^2 - 3
+      func1 = &Nx.add(Nx.pow(&1, 2), -3)
+
+      # f(x) = x^2 + x - 2
+      func2 = &Nx.add(Nx.pow(&1,2), Nx.add(&1, -2))
+
+      epochs1 = 5
+      func1_root_seq1 = R.newton_raphson(func1, 0.5, epochs1)
+      func1_root_seq2 = R.newton_raphson(func1, -0.5, epochs1)
+
+      assert equal_within_epsilon(func1_root_seq1[epochs1], Nx.sqrt(3))
+      assert equal_within_epsilon(func1_root_seq2[epochs1], Nx.negate(Nx.sqrt(3)))
+
+      epochs2 = 10
+      func2_root_seq1 = R.newton_raphson(func2, 0, epochs2)
+      func2_root_seq2 = R.newton_raphson(func2, -1, epochs2)
+
+      assert equal_within_epsilon(func2_root_seq1[epochs2], 1)
+      assert equal_within_epsilon(func2_root_seq2[epochs2], -2)
+    end
+  end
+end
