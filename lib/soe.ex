bba8a60e7bf29f6e527c56c05bd58a7c03d4def4
Santiago Imelio <santiago.imelio@mimiquate.com>
2024-09-20 16:48:48 -0300

    refactor Metnum.SOE, fix gha workflow

--- lib/soe.ex
+++ lib/soe.ex
@@ -5,18 +5,19 @@ defmodule Metnum.SOE do
 
   import Nx.Defn
 
-  jacobi_opts_schema = [
+  iterative_method_opts_schema = [
     tolerance: [
       doc: "The given tolerance of the solution of the system.",
       default: Nx.Constants.epsilon(:f32)
     ],
     max_epochs: [
-      doc: "Maximum of iterations for Jacobi method.",
+      doc: "Maximum of iterations for the iterative method.",
       default: 100
     ]
   ]
 
-  @jacobi_opts_schema NimbleOptions.new!(jacobi_opts_schema)
+  @iterative_method_opts_schema NimbleOptions.new!(iterative_method_opts_schema)
+  @methods [jacobi: 1, gauss_seidel: 2]
 
   def jacobi(
         %Nx.Tensor{} = a,
@@ -24,12 +25,29 @@ defmodule Metnum.SOE do
         %Nx.Tensor{} = x_0,
         opts \\ []
       ) do
-    opts = NimbleOptions.validate!(opts, @jacobi_opts_schema)
+    opts =
+      opts
+      |> NimbleOptions.validate!(@iterative_method_opts_schema)
+      |> Keyword.put(:method, @methods[:jacobi])
 
-    jacobi_n(a, b, x_0, opts)
+    iterative_method_n(a, b, x_0, opts)
   end
 
-  defnp jacobi_n(a, b, x, opts) do
+  def gauss_seidel(
+        %Nx.Tensor{} = a,
+        %Nx.Tensor{} = b,
+        %Nx.Tensor{} = x_0,
+        opts \\ []
+      ) do
+    opts =
+      opts
+      |> NimbleOptions.validate!(@iterative_method_opts_schema)
+      |> Keyword.put(:method, @methods[:gauss_seidel])
+
+    iterative_method_n(a, b, x_0, opts)
+  end
+
+  defnp iterative_method_n(a, b, x, opts) do
     {n} = Nx.shape(x)
 
     max_epochs = opts[:max_epochs]
@@ -41,29 +59,41 @@ defmodule Metnum.SOE do
     # put x_0 in the sequence
     sol_seq = Nx.put_slice(sol_seq, [0, 0], Nx.broadcast(x, {1, n}))
 
-    {sequence, _} =
-      while {sol_seq, {i = 0, a, b}}, not end_jacobi?(sol_seq, i, tol, max_epochs) do
-        x_new = jacobi_step(a, b, sol_seq[i])
+    method = Nx.broadcast(:nan, {opts[:method]})
 
-        {Nx.put_slice(sol_seq, [i + 1, 0], x_new), {i + 1, a, b}}
+    {sequence, _} =
+      while {sol_seq, {i = 0, a, b, method}}, not end_iterative?(sol_seq, i, tol, max_epochs) do
+        x_new = iterative_method_step(a, b, sol_seq[i], method)
+
+        {Nx.put_slice(sol_seq, [i + 1, 0], x_new), {i + 1, a, b, method}}
       end
 
     sequence
   end
 
-  defnp end_jacobi?(sequence, i, tol, max_epochs) do
+  defnp iterative_method_step(a, b, x_prev, method_tensor) do
+    {method} = Nx.shape(method_tensor)
+
+    cond do
+      Nx.equal(method, @methods[:jacobi]) -> jacobi_step(a, b, x_prev)
+      Nx.equal(method, @methods[:gauss_seidel]) -> gauss_seidel_step(a, b, x_prev)
+      true -> raise "method not found"
+    end
+  end
+
+  defnp end_iterative?(sequence, i, tol, max_epochs) do
     if i != 0 do
       prev_solution = sequence[i - 1]
       curr_solution = sequence[i]
 
-      i > max_epochs or l2_norm(prev_solution, curr_solution) < tol
+      i > max_epochs or l2_norm(prev_solution - curr_solution) < tol
     else
       0
     end
   end
 
-  deftransformp l2_norm(x, y) do
-    Nx.subtract(x, y) |> Nx.LinAlg.norm(ord: 2)
+  deftransformp l2_norm(x) do
+    Nx.LinAlg.norm(x, ord: 2)
   end
 
   defnp jacobi_step(a, b, x_prev) do
@@ -92,63 +122,6 @@ defmodule Metnum.SOE do
       end
 
     Nx.broadcast(x_new, {1, n})
-  end
-
-  gauss_seidel_opts_schema = [
-    tolerance: [
-      doc: "The given tolerance of the solution of the system.",
-      default: Nx.Constants.epsilon(:f32)
-    ],
-    max_epochs: [
-      doc: "Maximum of iterations for Gauss-Seidel method.",
-      default: 100
-    ]
-  ]
-
-  @gauss_seidel_opts_schema NimbleOptions.new!(gauss_seidel_opts_schema)
-
-  def gauss_seidel(
-        %Nx.Tensor{} = a,
-        %Nx.Tensor{} = b,
-        %Nx.Tensor{} = x_0,
-        opts \\ []
-      ) do
-    opts = NimbleOptions.validate!(opts, @gauss_seidel_opts_schema)
-
-    gauss_seidel_n(a, b, x_0, opts)
-  end
-
-  defnp gauss_seidel_n(a, b, x, opts) do
-    {n} = Nx.shape(x)
-
-    max_epochs = opts[:max_epochs]
-    tol = opts[:tolerance]
-
-    # sequence filler tensor
-    sol_seq = Nx.broadcast(:nan, {max_epochs, n})
-
-    # put x_0 in the sequence
-    sol_seq = Nx.put_slice(sol_seq, [0, 0], Nx.broadcast(x, {1, n}))
-
-    {sequence, _} =
-      while {sol_seq, {i = 0, a, b}}, not end_gauss_seidel?(sol_seq, i, tol, max_epochs) do
-        x_new = gauss_seidel_step(a, b, sol_seq[i])
-
-        {Nx.put_slice(sol_seq, [i + 1, 0], x_new), {i + 1, a, b}}
-      end
-
-    sequence
-  end
-
-  defnp end_gauss_seidel?(sequence, i, tol, max_epochs) do
-    if i != 0 do
-      prev_solution = sequence[i - 1]
-      curr_solution = sequence[i]
-
-      i > max_epochs or l2_norm(prev_solution, curr_solution) < tol
-    else
-      0
-    end
   end
 
   defnp gauss_seidel_step(a, b, x_prev) do
